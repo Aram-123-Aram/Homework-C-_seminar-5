d3e287f4e7789aae28c1ec3b47867cea290e4098
Aram <armsir-8182@mail.ru>
2022-11-05 10:40:44 +0300

    Finding Summa of nechotnix elements in massive

---
+++ Exercise 36_sem5/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int[] array = new int[8];
+
+int Sum_nech_el = 0;
+Console.Write("array= [");
+for(int i = 0; i < array.Length; i++ )
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write(array[i] + ", ");
+    
+    if(i % 2 != 0) {Sum_nech_el = Sum_nech_el + array[i]; }
+}
+Console.Write("] -> " + Sum_nech_el);
